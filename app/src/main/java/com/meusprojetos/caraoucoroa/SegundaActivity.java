0210c0d849a2977a4d725430f9edcc73496a537e
Ana Luisa Dias <analudias@live.com>
2020-08-29 23:07:55 -0300

    implementando função do botão jogar e voltar

--- app/src/main/java/com/meusprojetos/caraoucoroa/SegundaActivity.java
+++ app/src/main/java/com/meusprojetos/caraoucoroa/SegundaActivity.java
@@ -2,13 +2,35 @@ package com.meusprojetos.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class SegundaActivity extends AppCompatActivity {
+
+    private ImageView imageMoeda;
+    private Button buttonVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        imageMoeda = findViewById(R.id.imageMoeda);
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+
+        Bundle dados = getIntent().getExtras();
+
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
